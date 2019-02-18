65ef7cc6ad3d5cd677dc22ba28424f0649d9e66f
frokenfreyja <freyjasigur@gmail.com>
2019-02-18 19:49:45 +0000

    .

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 CREATE TABLE applications (
-  /* todo skema */
+  id serial primary key,
+  name varchar(128) not null,
+  email varchar(256) not null,
+  phone int not null,
+  text text,
+  job varchar(32) not null,
+  processed boolean default false,
+  created timestamp with time zone not null default current_timestamp,
+  updated timestamp with time zone not null default current_timestamp
 );
 
 CREATE TABLE users (
